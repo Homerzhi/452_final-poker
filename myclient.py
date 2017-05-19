4eac9b77a3608ab2ab8cbbfc9c06bfa685cb5961
Homerzhi <hua@csu.fullerton.edu>
2017-05-19 08:23:52 -0700

    Add files via upload
    using Diffie Helman key exchange.

--- myclient.py
+++ myclient.py
@@ -4,7 +4,7 @@ from Crypto.PublicKey import RSA
 from Crypto import Random
 
 from Crypto.Cipher import ARC4
-
+from random import randint
 #Generate private and public keys
 key=Random.new().read
 pr=RSA.generate(1024,key)
@@ -18,8 +18,8 @@ s=socket.socket()
 s.connect((host,port))   #connect takes a turple of a host address and port.
 
 
-message=pb.exportKey()
-s.send('client_key='+message+'\n')
+cl_publickey=pb.exportKey()
+s.send('client_key='+cl_publickey+'\n')
 print 'client sent public key'
 
 data=s.recv(1024)
@@ -28,12 +28,22 @@ data=data.replace('\r\n','')
 spb=RSA.importKey(data)
 print 'client received server public key'
 
-sessionkey=s.recv(1024)
-sessionkey=eval(sessionkey)
-sessionkey=pr.decrypt(sessionkey)
-print "client received sessionkey:",sessionkey
+DHkey=s.recv(1024)
+DHkey=eval(DHkey)
+DHkey=pr.decrypt(DHkey)
+aqy=map(int, DHkey.split())
+x=randint(3,aqy[1])
+y=aqy[0]**x%aqy[1]            #public key, need to send 
 
-message='-ready to receive numbers-'
+#session_key=(server_public_key)**(own_private_key)%q
+sessionkey=str(aqy[2]**x%aqy[1])
+print "client received a,q,y:",aqy, " and compute session key:",sessionkey
+
+DH_public_key=str(y)
+DH_public_key=str(spb.encrypt(DH_public_key,32))
+s.send(DH_public_key)
+
+message='ready to receive'
 while message!='q':
 
     sk= ARC4.new(sessionkey)
@@ -49,8 +59,12 @@ while message!='q':
     recvdata=sk.decrypt(recvdata)
               
     recvdata=map(int, recvdata.split())
-    data=max(recvdata)
-    print "recieved from ", s.getsockname(), " :"+str(recvdata)
+    if recvdata:
+        data=max(recvdata)
+        print "recieved from ", s.getsockname(), " :"+str(recvdata)
+    else:
+        data=0
+        print 'recieved nothing'
     message=raw_input("Press Enter to continoue->")
     message=str(data)
     
--- myserver.py
+++ myserver.py
@@ -2,7 +2,10 @@ import socket
 import select
 import sys
 import Queue
+import sympy  #for generate prime number
+
 from random import randint
+from random import shuffle
 import signal
 
 from Crypto.PublicKey import RSA
@@ -17,6 +20,7 @@ pb=pr.publickey()
 
         
 #def main():
+DH=[]
 clients={}
 clientnames=[]
 clientsessionkey={}
@@ -34,7 +38,7 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((host,port))   #bind takes a turple of a host address and port.
 
 #listen for incoming connections by given number
-server.listen(2)
+server.listen(3)
 # Trap keyboard interrupts
 
 
@@ -54,6 +58,29 @@ def sighandler(signum, frame):
     
 signal.signal(signal.SIGINT, sighandler)
 
+def dh_a(q):
+    a=primitiveRoots(q)
+    shuffle(a)
+    return a[0]      #now we have Diffie-Hellman two publicly known numbers.
+
+
+
+def permutation_check(a,q):
+    computed_set =set()        
+    for exponent in range (1, q):
+        computed_set.add((pow(a, exponent) % q))
+        if len(computed_set)<exponent:
+            return False
+    return True
+
+def primitiveRoots(q):
+    roots = []
+    for a in range(1, q):
+        if permutation_check(a,q):
+            roots.append(a)     
+    return roots
+
+
 
 #receive and send
 while inputs:
@@ -69,18 +96,36 @@ while inputs:
             clients[connection.getpeername()[1]]=[]   
             clientnames.append(connection.getpeername()[1])
             
+            #send server public key
             connection.send('public_key='+pb.exportKey()+'\n')
+            print 'server send RSA public key to client'
+            
+            
+            #receive client public key
             data=connection.recv(1024)
             data=data.replace("client_key=",'')
             data=data.replace('\r\n','')
             clientPublicKey[connection.getpeername()] =data
+            print 'server received client ',connection.getpeername()[1],' RSA public key'
             
-            #todo try to send session key
-            sessionkey=str(randint(10000,155555))    #here can use random, but keep difficult we use 1234
-            clientsessionkey[connection.getpeername()]=sessionkey
+            #todo try to send a, q, y
+            q= sympy.prime(randint(10,100))
+            a= dh_a(q)
+            x= randint(3,q)         #private
+            y=a**x%q                #public key, need to send 
+            print 'server send client: ',connection.getpeername()[1],'Diffie-Hellman a=',a,',q=',q,',y=',y,' encrypted by client ',connection.getpeername()[1],' RSA public key' 
+            #session_key=(client_public_key)**(own_private_key)%q
+            DHellman=str(a)+' '+str(q)+' '+str(y)    #here can use random, but keep difficult we use 1234            
             client_publickey=RSA.importKey(clientPublicKey[connection.getpeername()])
-            sessionkey=str(client_publickey.encrypt(sessionkey,32))
-            connection.send(sessionkey)
+            DHellman=str(client_publickey.encrypt(DHellman,32))
+            connection.send(DHellman)
+            
+            DH_client_pu_key=connection.recv(1024)
+            DH_client_pu_key=eval(DH_client_pu_key)
+            DH_client_pu_key=pr.decrypt(DH_client_pu_key)
+            sessionkey=str(int(DH_client_pu_key)**x%q)
+            print 'server received client: ',connection.getpeername()[1],' pu:',DH_client_pu_key,' and compute seesion key:',sessionkey
+            clientsessionkey[connection.getpeername()]=sessionkey
             
             
             connection.setblocking(0)
@@ -121,13 +166,14 @@ while inputs:
                     if len(clients[clientnames[0]])==3 and len(clients[clientnames[1]])==3:
                         max1=max( map(int, clients[clientnames[0]]))
                         max2=max( map(int, clients[clientnames[1]]))
+                        print  clientnames[0],':',max1, '; ',clientnames[1] ,':',max2
                         if max1 > max2:
                             print clientnames[0], ' win'
                         elif max1==max2:
                             print clientnames[0], ' and ', clientnames[1], ' are even'
                         else:
                             print clientnames[1], ' win'
-                            
+                        sys.exit()
                         break
 
             
